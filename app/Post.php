7af44875a396c4b7afdfb68cfd057009150bb9de
izumitomo <luckystar.tomosuke@gmail.com>
2022-05-07 13:58:00 +0900

    419エラー修正

--- app/Post.php
+++ app/Post.php
@@ -2,12 +2,13 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use SoftDeletes;
+    
     protected $fillable = [
         "title",
         "body",
